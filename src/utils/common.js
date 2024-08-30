e89ded3e1bf2cba4b1212052085e9273d42e6a88
Kaushik Jain <kaushik.jain@intimetec.com>
2024-08-30 11:32:40 +0530

    deleted src files for code review

--- src/utils/common.js
+++
@@ -1,7 +0,0 @@
-import { API_OPTIONS } from "./constants";
-
-export const getTMDBApiData = async (url) => {
-  const data = await fetch(url, API_OPTIONS);
-  const json = await data.json();
-  return json.results;
-};
